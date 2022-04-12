e804b1a80d006fd07eeadfaf07fd83f6c98b8901
rockdev <19998916+rockdev101@users.noreply.github.com>
2022-04-12 10:52:42 +0900

    CW721-base

--- contracts/nftmx_multi_level_royalty/src/error.rs
+++ contracts/nftmx_multi_level_royalty/src/error.rs
@@ -1,6 +1,5 @@
 use cosmwasm_std::StdError;
 use thiserror::Error;
-use cosmwasm_std::Uint128;
 
 #[derive(Error, Debug, PartialEq)]
 pub enum ContractError {
@@ -10,58 +9,12 @@ pub enum ContractError {
     #[error("Unauthorized")]
     Unauthorized {},
 
-    #[error("Token Id are different")]
-    TokenIdDifferent {},
+    #[error("token_id already claimed")]
+    Claimed {},
 
-    #[error("NFT address are not matched")]
-    NotMatchNFT {},
+    #[error("Cannot set approval that is already expired")]
+    Expired {},
 
-    #[error("Price is not matched")]
-    NotMatchPrice {},
-
-    #[error("Protection Rate is not valid")]
-    InvalidProtectionRate {},
-
-    #[error("Protection time is different")]
-    InvalidProtectionTime {},
-
-    #[error("typeorder should be AuctionType")]
-    NoAuctionType{},
-
-    #[error("typeorder should be FixedPay")]
-    NoFixedPay{},
-
-    #[error("This order should be done by buyer")]
-    InvalidBuyer{},
-
-    #[error("Sell order is still alive")]
-    SellOrderAlive{},
-
-    #[error("Insufficient offer time")]
-    InsufficientOfferTime{},
-
-    #[error("Protection time expired")]
-    ExpiredProtectionTime{},
-
-    #[error("Price is not matched")]
-    PriceNotMatch{},
-
-    #[error("Invalid coin amount")]
-    InvalidZeroAmount{},
-
-    #[error("Funding is ended")]
-    FundingIsEnded{},
-    
-    #[error("Minting cannot exceed the cap")]
-    CannotExceedCap{},
-
-    #[error("Funding is ended.")]
-    FundingEnded{},
-    
-    #[error("Balance Insufficient")]
-    BalanceInsufficient{},
-
-    #[error("Pool Balance is Insufficient")]
-    PoolBalanceInsufficient{},
-
+    #[error("Approval not found for: {spender}")]
+    ApprovalNotFound { spender: String },
 }
--- contracts/nftmx_multi_level_royalty/src/state.rs
+++ contracts/nftmx_multi_level_royalty/src/state.rs
@@ -1,68 +1,140 @@
 use schemars::JsonSchema;
+use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
-use cosmwasm_std::{ Addr };
-use cw_storage_plus::{ Item, Map };
-use cosmwasm_std:: { Uint256, Decimal, Timestamp, Uint128 };
-use crate::asset::{Asset};
-use std:: { fmt };
+use std::marker::PhantomData;
 
+use cosmwasm_std::{Addr, BlockInfo, StdResult, Storage};
 
-#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
-pub struct PackableNFT {
-    pub token_id: String,
-    pub token_name: String,
-    pub token_uri: String,
-    pub minted_by: Addr,
-    pub current_owner: Addr,
-    pub previous_owner: Addr,
-    pub price: Asset,
-    pub number_of_transfers: Uint128,
-    pub for_sale: bool,
+use cw721::{ContractInfoResponse, CustomMsg, Cw721, Expiration};
+use cw_storage_plus::{Index, IndexList, IndexedMap, Item, Map, MultiIndex};
+
+pub struct Cw721Contract<'a, T, C>
+where
+    T: Serialize + DeserializeOwned + Clone,
+{
+    pub contract_info: Item<'a, ContractInfoResponse>,
+    pub minter: Item<'a, Addr>,
+    pub token_count: Item<'a, u64>,
+    /// Stored as (granter, operator) giving operator full control over granter's account
+    pub operators: Map<'a, (&'a Addr, &'a Addr), Expiration>,
+    pub tokens: IndexedMap<'a, &'a str, TokenInfo<T>, TokenIndexes<'a, T>>,
+
+    pub(crate) _custom_response: PhantomData<C>,
+}
+
+// This is a signal, the implementations are in other files
+impl<'a, T, C> Cw721<T, C> for Cw721Contract<'a, T, C>
+where
+    T: Serialize + DeserializeOwned + Clone,
+    C: CustomMsg,
+{
+}
+
+impl<T, C> Default for Cw721Contract<'static, T, C>
+where
+    T: Serialize + DeserializeOwned + Clone,
+{
+    fn default() -> Self {
+        Self::new(
+            "nft_info",
+            "minter",
+            "num_tokens",
+            "operators",
+            "tokens",
+            "tokens__owner",
+        )
+    }
+}
+
+impl<'a, T, C> Cw721Contract<'a, T, C>
+where
+    T: Serialize + DeserializeOwned + Clone,
+{
+    fn new(
+        contract_key: &'a str,
+        minter_key: &'a str,
+        token_count_key: &'a str,
+        operator_key: &'a str,
+        tokens_key: &'a str,
+        tokens_owner_key: &'a str,
+    ) -> Self {
+        let indexes = TokenIndexes {
+            owner: MultiIndex::new(token_owner_idx, tokens_key, tokens_owner_key),
+        };
+        Self {
+            contract_info: Item::new(contract_key),
+            minter: Item::new(minter_key),
+            token_count: Item::new(token_count_key),
+            operators: Map::new(operator_key),
+            tokens: IndexedMap::new(tokens_key, indexes),
+            _custom_response: PhantomData,
+        }
+    }
+
+    pub fn token_count(&self, storage: &dyn Storage) -> StdResult<u64> {
+        Ok(self.token_count.may_load(storage)?.unwrap_or_default())
+    }
+
+    pub fn increment_tokens(&self, storage: &mut dyn Storage) -> StdResult<u64> {
+        let val = self.token_count(storage)? + 1;
+        self.token_count.save(storage, &val)?;
+        Ok(val)
+    }
+
+    pub fn decrement_tokens(&self, storage: &mut dyn Storage) -> StdResult<u64> {
+        let val = self.token_count(storage)? - 1;
+        self.token_count.save(storage, &val)?;
+        Ok(val)
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct TokenInfo<T> {
+    /// The owner of the newly minted NFT
+    pub owner: Addr,
+    /// Approvals are stored here, as we clear them all upon transfer and cannot accumulate much
+    pub approvals: Vec<Approval>,
+
+    /// Universal resource identifier for this NFT
+    /// Should point to a JSON file that conforms to the ERC721
+    /// Metadata JSON Schema
+    pub token_uri: Option<String>,
+
+    /// You can add any custom metadata here when you extend cw721-base
+    pub extension: T,
 }
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
-pub struct NftPack {
-    pub pack_id: Uint256,
-    pub pack_name: String,
-    pub item_count: u64,
-    pub pack_items: Vec<Uint256>,
-    pub minted_by: Addr,
-    pub current_owner: Addr,
-    pub previous_owner: Addr,
-    pub number_of_transfers: Uint128,
-    pub for_sale: bool,
-    pub royalty_owners: Vec<Addr>,
-    //TODO royalty_fees map
+pub struct Approval {
+    /// Account that can transfer/send the token
+    pub spender: Addr,
+    /// When the Approval expires (maybe Expiration::never)
+    pub expires: Expiration,
 }
 
-#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
-pub struct TokenPack {
-    pub pack_id: Uint256,
-    pub pack_name: String,
-    pub token_address: Addr,
-    pub token_amount: Uint256,
-    pub minted_by: Addr,
-    pub current_owner: Addr,
-    pub previous_owner: Addr,
-    pub current_price: Asset,
-    pub previous_price: Asset,
-    pub number_of_transfers: Uint128,
-    pub for_sale: bool,
-    pub royalty_owners: Vec<Addr>,
-    //TODO royalty_fees map
+impl Approval {
+    pub fn is_expired(&self, block: &BlockInfo) -> bool {
+        self.expires.is_expired(block)
+    }
 }
 
-pub const PACKABLENFTS: Map<&String, PackableNFT> = Map::new("all_packable_nfts");
-pub const NFTPACKS: Map<&Uint256, NftPack> = Map::new("all_nft_packs");
-pub const NFTPACKOWNERS: Map<&Uint256, Addr> = Map::new("nft_pack_owners");
-pub const NFTPACKBALANCE: Map<&Addr, Uint256> = Map::new("nft_pack_balance");
-pub const NFTPACKAPPROVALS: Map<&Uint256, Addr> = Map::new("nft_pack_approvals");
+pub struct TokenIndexes<'a, T>
+where
+    T: Serialize + DeserializeOwned + Clone,
+{
+    pub owner: MultiIndex<'a, Addr, TokenInfo<T>, Addr>,
+}
 
-pub const TOKENPACKS: Map<&Uint256, TokenPack> = Map::new("token_packs");
-pub const TOKENPACKOWNERS: Map<&Uint256, Addr> = Map::new("token_pack_owners");
-pub const TOKENPACKBALANCE: Map<&Addr, Uint256> = Map::new("token_pack_balance");
-pub const TOKENPACKAPPROVALS: Map<&Uint256, Addr> = Map::new("token_pack_approvals");
+impl<'a, T> IndexList<TokenInfo<T>> for TokenIndexes<'a, T>
+where
+    T: Serialize + DeserializeOwned + Clone,
+{
+    fn get_indexes(&'_ self) -> Box<dyn Iterator<Item = &'_ dyn Index<TokenInfo<T>>> + '_> {
+        let v: Vec<&dyn Index<TokenInfo<T>>> = vec![&self.owner];
+        Box::new(v.into_iter())
+    }
+}
 
-pub const TOKENAMEEXISTS: Map<&String, bool> = Map::new("token_name_exists");
-pub const TOKEURIEXISTS: Map<&String, bool> = Map::new("token_uri_exists");
-pub const PACKNAMEEXISTS: Map<&String, bool> = Map::new("pack_name_exists");
+pub fn token_owner_idx<T>(d: &TokenInfo<T>) -> Addr {
+    d.owner.clone()
+}
